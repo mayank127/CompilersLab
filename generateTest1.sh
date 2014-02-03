fb82d182c2d17811a8ee896963f4db35be487c98
Mayank Meghwanshi <mayankmeghwanshi@gmail.com>
2014-02-03 14:38:16 +0530

    moved all test cases

--- generateTest1.sh
+++ generateTest1.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-FILES="ContDoWhile.c GlobTernaryInIfLoop.c  MultRelInTernary.c RelInCond.c RelInStmt.c test.c Glob.c iftmp.c MultRel.c NestedIf.c NotEqInIf.c RelInIfNotEq.c TempInIfRel.c TwoRelInTernary.c"
+FILES="ContDoWhile.c  Glob.c            GlobNoDeclNoDef1BB.c   MultRelInTernary.c  NoDeclNoDef1BB.c  RelInIfNotEq.c  test.c \
+Decl1BB.c      GlobDeclDef.c     GlobTernaryInIfLoop.c  MultGlob.c      NestedIf.c          NotEqInIf.c       RelInStmt.c     TwoRelInTernary.c \
+DeclDef1BB.c   GlobLocSameVar.c  iftmp.c                MultRel.c         RelInCond.c       TempInIfRel.c   VarMain.c"
+
 for f in $FILES
 do
 	echo $f
 	make -f Makefile.cfg FILE=$f
 done
 
-FOLDER=./test_files/1
+FOLDER=./test_files
 
 for f in $FILES
 do
@@ -14,7 +17,7 @@ do
 	./cfglp -tokens -ast -eval $FOLDER/$f"s306.cfg"
 done
 
-FILES=./test_files/1/*.ecfg
+FILES=./test_files/*.ecfg
 for f in $FILES
 do
 	echo $f
