efa7a4ceb3a7c47614baffa0ac707a5ff42def58
Mayank Meghwanshi <mayankmeghwanshi@gmail.com>
2014-03-10 14:16:11 +0530

    adding more test files

--- generateTest1.sh
+++ generateTest1.sh
@@ -6,23 +6,12 @@ EFILES=./test_files/*.ecfg
 for f in $EFILES
 do
 	echo $f >>out1
-	echo $f >> out2
 	./cfglp -eval -ast -tokens  -d $f >>out1
+	echo $f >> out2
 	./cfglp64 -tokens -eval -ast  -d $f >>out2
 done
 
-FILES="ArithBrckt.c       Decl1BB.c         fun_arg_arth_rel.c    IfFloatRelArith.c     MultRelInTernary.c  PassParamRet.c   TempInIfRel.c \
-Arith.c            DeclDef1BB.c      Glob.c                IfIntFloat.c          NestedIf.c          recursion.c      test.c \
-ArithFloat.c       DTemp.c           GlobDeclDef.c         IfIntFloatRelArith.c  recursivecall.c  TwoRelInTernary.c \
-ArithIntFloat.c    EmptyBB.c         GlobInMid.c           IfRelArith.c          NoDeclNoDef1BB.c    RelArithBrckt.c  TypeCast.c \
-ArithRelFn.c       EmptyFn.c         GlobLocSameVar.c      iftmp.c               NoFnCall.c          RelArith.c       Unary.c \
-ArithRelInOutFn.c  EmptyIf.c         GlobNoDeclNoDef1BB.c  IfUnary.c             NotEqInIf.c         RelInCond.c      UnaryPlus.c \
-Brckt.c            Fact.c            GlobPassSameParam.c   IntInFloat.c          OperationParam.c    RelInStmt.c      UserIfTmp.c \
-CatchFloatInInt.c  FloatRelArith.c   GlobRemov.c           mul_fun.c             OperationRet.c      RetInFor.c       VarMain.c \
-CatchRet.c         FloatUnary.c      GlobUse.c             MultFn.c              ParamActualSame.c   return_float.c   VoidFnPassParam.c \
-Const.c            fn_call_return.c  IfArith.c             MultGlob.c            PassParam.c         RetVal.c         void_function.c \
-ContDoWhile.c      FnPrtyp.c         IfFloatArith.c        MultRel.c             PassParamDiff.c     t2.c             VoidRet.c \
-"
+cd test_files/; FILES=$(ls *.c); cd ..;
 
 for f in $FILES
 do
@@ -31,13 +20,14 @@ do
 done
 
 FOLDER=./test_files
-
+echo "" > out3
+echo "" > out4
 for f in $FILES
 do
-	echo $f"s306.cfg" >> out1
-	echo $f"s306.cfg" >> out2
-	./cfglp -eval -ast -tokens -d $FOLDER/$f"s306.cfg" >> out1
-	./cfglp64 -tokens -eval -ast -d $FOLDER/$f"s306.cfg" >> out2
+	echo $f"s306.cfg" >> out3
+	./cfglp -eval -ast -tokens -d $FOLDER/$f"s306.cfg" >> out3
+	echo $f"s306.cfg" >> out4
+	./cfglp64 -tokens -eval -ast -d $FOLDER/$f"s306.cfg" >> out4
 done
 
 # meld out1 out2
