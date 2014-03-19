863dc1bdc664aad04bf4ae7cc822b9522bdb5e7b
Mayank Meghwanshi <mayankmeghwanshi@gmail.com>
2014-03-19 18:37:37 +0530

    final submission for level 1c

--- generateTest1.sh
+++ generateTest1.sh
@@ -6,9 +6,9 @@ EFILES=./test_files/*.ecfg
 for f in $EFILES
 do
 	echo $f"" >>out1
-	./cfglp -icode $f >>out1
-	# echo $f"" >> out2
-	# ./cfglp64 -ast -tokens -eval  -d $f >>out2
+	./cfglp -icode -d $f >>out1
+	echo $f"" >> out2
+	./cfglp64 -icode -d  $f >>out2
 done
 
 cd test_files/; FILES=$(ls *.c); cd ..;
@@ -16,7 +16,7 @@ cd test_files/; FILES=$(ls *.c); cd ..;
 for f in $FILES
 do
 	echo $f
-	# make -f Makefile.cfg FILE=$f
+	make -f Makefile.cfg FILE=$f
 done
 
 FOLDER=./test_files
@@ -25,9 +25,9 @@ echo "" > out4
 for f in $FILES
 do
 	echo $f"s306.cfg" >> out3
-	./cfglp -icode  $FOLDER/$f"s306.cfg" >> out3
-	# echo $f"s306.cfg" >> out4
-	# ./cfglp64 -icode -d $FOLDER/$f"s306.cfg" >> out4
+	./cfglp -icode -d $FOLDER/$f"s306.cfg" >> out3
+	echo $f"s306.cfg" >> out4
+	./cfglp64 -icode  -d $FOLDER/$f"s306.cfg" >> out4
 done
 
 # meld out1 out2
